bc85a477aaf4276d4cbf0739518094267f739c0e
Jeff Yang <jeffyang@Jeffs-MacBook-Pro-4.local>
2018-12-13 16:23:01 -0600

    greet new user

--- server/server.js
+++ server/server.js
@@ -12,19 +12,32 @@ var io = socketIO(server);
 io.on("connection", (socket) => {
   console.log("New user connected");
 
-  // socket.emit("newMessage", {
-  //   from: "bob@example.com",
-  //   text: "hello, whatsup",
-  //   createdAt: 123
-  // });
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
 
   socket.on("createMessage", (message) => {
     console.log("createMessage", message);
-    io.emit("newMessage", {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+
+    // io.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
+    // socket.broadcast.emit("newMessage", {
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    // });
   });
 
   socket.on("disconnect", () => {
