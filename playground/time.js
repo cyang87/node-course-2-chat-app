fd7945d647f9216a28d18e9d1c7de7b930c7283a
Jeff Yang <jeffyang@Jeffs-MacBook-Pro-4.local>
2018-12-17 16:42:00 -0600

    time stamp

---
+++ playground/time.js
@@ -0,0 +1,15 @@
+var moment = require("moment");
+
+// var date = new Date();
+// console.log(date.getMonth());
+
+var date = moment();
+date.add(1, "years").subtract(9, "months");
+console.log(date.format("MMM Do, YYYY"));
+
+//10:35 am
+// 6:01 am
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format("h:mm a"));
